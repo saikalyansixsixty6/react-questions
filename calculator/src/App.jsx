9174723cab1b9399c6cd8cadc86cb09ad4e98ca7
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-10-02 14:36:25 +0000

    done with calculator logic

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -7,7 +7,11 @@ function App() {
   const [input2,setInput2] = useState()
 
   const [result,setResult] = useState()
-  const [operator,setOperator] = useState()
+  const [operator,setOperator] = useState("+")
+  const [count,setCount] = useState(0)
+
+
+
   const handler1 = (e) =>{
        setInput1(e.target.value)
   }
@@ -21,26 +25,53 @@ function App() {
       setInput2("");
       setResult();
       setOperator("+");
+      
   }
+
+  
+  const plusOperator = () =>{
+    setCount(count+1);
+    setOperator("+");
+    setResult(parseInt(input1)+parseInt(input2))
+  }
+
+  const minusOperator = () =>{
+    setCount(count+1);
+    setOperator("-")
+    setResult(input1-input2)
+  }
+
+  const multiplyOperator = () =>{
+    setCount(count+1);
+    setOperator("*");
+    setResult(input1*input2)
+  }
+  const divideOperator = () =>{
+    setCount(count+1);
+    setOperator("/")
+    setResult(input1/input2)
+  }
+  
   return (
     <> 
       <h1>Calculator</h1>
       <div>
+      <h1>Number of Operations:{count}</h1>
       <input className='inputEle' type='number' value={input1} onChange={handler1} />
-      <button>+</button>
+      <button>{operator}</button>
       <input className='inputEle' type='number' value={input2} onChange={handler2}/>
       </div>
 
       <div className='buttons'>
-           <button className='button'>+</button>
-           <button className='button'>-</button>
-           <button className='button'>*</button>
-           <button className='button'>/</button>
+           <button className='button' onClick={plusOperator}>+</button>
+           <button className='button' onClick={minusOperator}>-</button>
+           <button className='button' onClick={multiplyOperator}>*</button>
+           <button className='button' onClick={divideOperator}>/</button>
       </div>
 
       <button className='resetButton' onClick={resetHandler}>Reset</button>
 
-      <h1 className='result'>Result:{input1},{input2}</h1>
+      <h1 className='result'>Result:{result}</h1>
 
     </>
   )
